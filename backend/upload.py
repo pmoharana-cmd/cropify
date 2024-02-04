f2933af2a330550483ff1700354e53195de8902e
pmoharana-cmd <pmoharana032474@gmail.com>
2024-02-04 01:06:55 -0500

    added database insert functionality

--- backend/upload.py
+++ backend/upload.py
@@ -1,2 +1,40 @@
+from pymongo import MongoClient, database
+import os
+from dotenv import load_dotenv
+import base64
+
+load_dotenv()
+
+USERNAME = os.getenv("MONGO_USERNAME")
+PASSWORD = os.getenv("MONGO_PASSWORD")
+
+
+client = MongoClient("mongodb+srv://%s:%s@cropdata.mdv0mdr.mongodb.net/?retryWrites=true&w=majority" % (USERNAME, PASSWORD))
+
 def uploadData(humidity: float, temp: float, water_level: float, height: float, imagePath: str):
-    return
+    if not os.path.exists(imagePath):
+        raise ValueError("Path does not exist")
+
+    row = {
+        "humidity": humidity,
+        "temperature": temp,
+        "water_level": water_level,
+        "height": height,
+        "image": convertImageToArray(imagePath)
+    }
+
+    db = client.main
+
+    crop_data = db.get_collection("cropData")
+
+    data_id = crop_data.insert_one(row)
+
+    return data_id
+
+
+def convertImageToArray(img: str):
+    with open(img, "rb") as image:
+        coded_string = base64.b64encode(image.read())
+        return coded_string
+    
+print(uploadData(0, 0, 0, 0, "picture.jpg"))
